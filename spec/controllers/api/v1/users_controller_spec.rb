9c82fec855120656eab35aff1a579bfe3c2d15a8
Arda Cicek <ardacck@gmail.com>
2015-05-13 19:09:58 +0300

    Adds authorization for the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -7,12 +7,14 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
-      delete :destroy, { id: @user.id }
+      api_authorization_header @user.auth_token #we added this line
+      delete :destroy, id: @user.auth_token
     end
 
     it { should respond_with 204 }
 
   end
+
 
   describe "GET #show" do
     before(:each) do
@@ -69,6 +71,7 @@ describe Api::V1::UsersController do
   describe "PUT/PATCH #update" do
     before(:each) do
       @user = FactoryGirl.create :user
+      request.headers['Authorization'] =  @user.auth_token
     end
 
     context "when is successfully updated" do
