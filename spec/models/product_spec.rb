b667cfa792d30165d26aaed7b0884c03510f8f33
Arda Cicek <ardacck@gmail.com>
2015-05-14 18:43:07 +0300

    Finishes modeling the product model along with user associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,12 +7,14 @@ describe Product do
   it { should respond_to(:title) }
   it { should respond_to(:price) }
   it { should respond_to(:published) }
-  it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
 
   # it { should not_be_published }
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :user }
+  it { should belong_to :user }
+
 end
